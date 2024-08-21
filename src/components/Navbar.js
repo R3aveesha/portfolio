111ede8108d77c307cb5ec1a952153d0ab7e7551
raveesha gimhan <146311393+R3aveesha@users.noreply.github.com>
2024-08-21 17:57:41 +0530

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Navbar.css'
 
-function Navbar() {
+function Navbar =()=> {
     return (
       <nav className='navbar'>
         <img src='' alt=''></img>
@@ -11,6 +11,7 @@ function Navbar() {
         </div>
         <button className='desktopMenuBtn'>
             <img src="" alt="" className='desktopMenuImg'/>
+
         </button>
       </nav>
     );

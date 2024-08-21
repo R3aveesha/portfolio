6c73bb598c876a86c9c239ba9181294ae645dae5
raveesha gimhan <146311393+R3aveesha@users.noreply.github.com>
2024-08-21 10:20:26 +0530

    updaeted

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Navbar.css'
+
+function Navbar() {
+    return (
+      <nav className='navbar'>
+        <img src='' alt=''></img>
+        <div className='desktopMenu'>
+
+
+        </div>
+        <button className='desktopMenuBtn'>
+            <img src="" alt="" className='desktopMenuImg'/>
+        </button>
+      </nav>
+    );
+}
+
+export default Navbar;

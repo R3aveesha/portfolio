232e12f5b850afe1de0e627d180011e20216e96d
raveesha gimhan <146311393+R3aveesha@users.noreply.github.com>
2024-08-23 00:18:00 +0530

    updated

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -1,21 +1,29 @@
-import React from 'react'
-import './intro.css'
-import myImage from '../../assets/myImage.png'
-import letterIcon from '../../assets/letterIcon.png'
-import {Link} from 'react-scroll'
+import React from 'react';
+import './intro.css';
+import myImage from '../../assets/myImage.png';
+import { Link } from 'react-scroll';
 
-function intro() {
+function Intro() {
   return (
-    <section id ='intro'>
-        <div className='introContent'>
-            <span className='hello'>Hello,</span>
-            <span className='introText'>I'm <span className='introName'>Raveesha</span><br/>Website Designer</span>
-            <p className='introPara'>I am a skilled full stack developer with experience in creating visually engaging and user-friendly web applications. My expertise spans both front-end and back-end development, allowing me to build seamless, responsive, and efficient digital solutions. I am proficient in various programming languages and frameworks, and I excel at transforming ideas into functional and appealing interfaces that enhance user experience."</p>
-           <Link> <button className='hireBtn'>Hire me</button></Link>
-        </div>
-        <img src={myImage} className='bg' alt='profile'></img>
+    <section id='intro'>
+      <div className='myImg'>
+      </div>
+      <div className='introContent'>
+        <span className='hello'>Hello,</span>
+        <span className='introText'>
+          I'm <span className='introName'>Raveesha</span><br />Website Designer
+        </span>
+        <p className='introPara'>
+          I am a skilled full stack developer with experience in creating visually engaging and user-friendly web applications. My expertise spans both front-end and back-end development, allowing me to build seamless, responsive, and efficient digital solutions. I am proficient in various programming languages and frameworks, and I excel at transforming ideas into functional and appealing interfaces that enhance user experience.
+        </p>
+        <Link to='contact'>
+          <button className='hireBtn'>Hire me</button>
+        </Link>
+
+        <img src={myImage} className='bg' alt='profile' />
+      </div>
     </section>
-  )
+  );
 }
 
-export default intro;
+export default Intro;

9caea9a724c62fb2af66f41fcb9284f1e727dbcb
raveesha gimhan <146311393+R3aveesha@users.noreply.github.com>
2024-08-22 20:42:58 +0530

    updaetd

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import './intro.css'
-
-
-
+import myImage from '../../assets/myImage.png'
+import letterIcon from '../../assets/letterIcon.png'
+import {Link} from 'react-scroll'
 
 function intro() {
   return (
@@ -11,11 +11,11 @@ function intro() {
             <span className='hello'>Hello,</span>
             <span className='introText'>I'm <span className='introName'>Raveesha</span><br/>Website Designer</span>
             <p className='introPara'>I am a skilled full stack developer with experiance in creating visual</p>
-            <button></button>
+            <Link><button className='btn'><img src={letterIcon} alt='hire me'/>Hire Me</button></Link>
         </div>
-        <img src='' className='bg' alt=''></img>
+        <img src={myImage} className='bg' alt='profile'></img>
     </section>
   )
 }
 
-export default intro
+export default intro;
